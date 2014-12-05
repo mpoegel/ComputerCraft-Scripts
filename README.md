8bc477576842981caf76ad7917c0b179f7625591
Matt Poegel <mpoegel@comcast.net>
2014-12-05 12:41:23 -0500

    restructing to hold all ComputerCraft scripts

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-ComputerCraft Controlled Tinker's Construct Smeltery
-------
-Computer controlled smeltery using ComputerCraft and Tinker's Construct (and other mod's as well) for use in the Feed the Beast Monster pack (or similiar)
+##ComputerCraft Scripts
+A collection of scripts I've written while playing ComputerCraft in Feed The Beast (Minecraft)
 
-####Version: 1.1
+###Scripts
+####1. Smeltery Controller
+This automates the process of using the Tinker's Construct Smeltery by:
+* managing input from/output to storage tanks for molten liquids to minimize waste.
+* offering a touchscreen interface to pour or drain the tank
+* fully customizable for the casts and molten ores in the systems using the config file
 
-ComputerCraft Version:
-FTB Moster Version:
 
-Grab the code in FTB from pastebin:
+####Notes
+I may often use `button.lua` to create a touchscreen. This was originally written by [direwolf20](http://www.youtube.com/direwolf20), but I may occasionally make modifications to it depending on what I want to do.
---
+++ Smeltery Controller/smeltery_config.lua
@@ -0,0 +1,71 @@
+---------------------------------------
+-- Smeltery Configuration
+--
+-- EDIT THIS FILE TO SETUP YOUR SMELTERY
+-- AND ONLY THIS FILE
+-- (unless you know what you're doing)
+---------------------------------------
+
+
+-- arguments can be:
+-- right, left, top, bottom, or back
+-- location of the advanced monitor relative
+-- to the computer
+monitor_loc = "back"
+-- location of the rednet cable that goes to
+-- the storage tanks
+storage_tanks = "left"
+-- location of the rednet cable that goes to
+-- the casting tables
+casting_tables = "right"
+-- location of the rednet modem connected to
+-- the tank computer
+rednet_modem = "bottom"
+-- location of the smeltery drain relative to
+-- the peripheral proxy
+tank_loc = "top"
+-- name of the smeltery drain peripheral
+-- (adjust the number suffix as needed!)
+drain_name = "tconstruct_smelterydrain_0"
+-- name of the request pipe peripheral
+-- (adjust the number suffix as needed!)
+req_pipe_name = "LogisticsPipes:Request_3"
+
+-- empty tank color
+empty_tank = colors.purple
+-- void tank color
+void_tank = colors.cyan
+
+
+-- list of all the liquids and their respective
+-- rednet cable colors
+liquids = {}
+-- liquid goes in the brackets as a string
+-- the value parameter is the cable color
+-- colors are:
+-- colors.white, .orange, ........
+liquids["Aluminum Brass"] = colors.white
+liquids["Molten Glass"] = colors.orange
+liquids["Molten Obsidian"] = colors.maganta
+liquids["Molten Bronze"] = colors.lightBlue
+liquids["Molten Tin"] = colors.yellow
+liquids["Molten Copper"] = colors.lime
+liquids["Molten Emerald"] = colors.pink
+liquids["Molten Steel"] = colors.gray
+liquids["Molten Cobalt"] = colors.lightGray
+liquids["Molten Ardite"] = colors.cyan
+
+
+-- list of all the pouring options and the 
+-- rednet cable colors
+-- format is the same as above
+ingot_color = colors.white
+block_color = colors.orange
+casts = {}
+casts["pickaxe head"] = colors.magenta
+casts["shovel head"] = colors.lightBlue
+casts["axe head"] = colors.yellow
+casts["tool rod"] = colors.lime
+casts["tool binding"] = colors.pink
+casts["sword blade"] = colors.gray
+casts["blank cast"] = colors.lightGray
